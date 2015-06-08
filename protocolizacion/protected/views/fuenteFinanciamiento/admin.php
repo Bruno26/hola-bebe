031c9c9d5e08d51bfc0361a8e60f42d7d52e7ebf
Lenin Hernandez <lenin@vitp2400>
2015-06-07 19:36:10 -0430

    ajustando grid fuente financiamiento

--- protocolizacion/protected/views/fuenteFinanciamiento/admin.php
+++ protocolizacion/protected/views/fuenteFinanciamiento/admin.php
@@ -23,7 +23,7 @@ return false;
 ");
 ?>
 
-<h1>Manage Fuente Financiamientos</h1>
+<h1>Gestión de Fuentes de Financiamientos</h1>
 
 <p>
 	You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>
@@ -31,7 +31,7 @@ return false;
 	or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button btn')); ?>
+<?php echo CHtml::link('Busqueda Avanzada','#',array('class'=>'search-button btn')); ?>
 <div class="search-form" style="display:none">
 	<?php $this->renderPartial('_search',array(
 	'model'=>$model,
@@ -46,10 +46,22 @@ return false;
 		'id_fuente_financiamiento',
 		'nombre_fuente_financiamiento',
 		'estatus',
+
+        array(
+            'name' => 'fecha_creacion',
+            'value' => 'Yii::app()->dateFormatter->format("d/M/y - hh:mm a", strtotime($data->fecha_creacion))',
+            'header' => 'Creación',
+        ),
+
+        'usuario_id_creacion' => array(
+            'header' => 'Usuario',
+            'name' => 'usuario_id_creacion',
+            //'value' => '$data->usuarioIdCreacion->username',
+            'filter' => CHtml::listData(CrugeStoredUser::model()->findall(), 'iduser', 'username'),
+        ),
+		/*
 		'fecha_creacion',
 		'fecha_actualizacion',
-		'usuario_id_creacion',
-		/*
 		'usuario_id_actualizacion',
 		*/
 array(
