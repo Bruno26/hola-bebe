637f0661f0a1212b673ec104ac52f5ab33132456
Desarollo Web <desarollo@desarrollo>
2015-06-17 18:08:13 -0430

    Subiendo Editar y GridView de Oficina y Abogado

--- protocolizacion/protected/views/oficina/admin.php
+++ protocolizacion/protected/views/oficina/admin.php
@@ -1,25 +1,25 @@
 <?php
-//$this->breadcrumbs = array(
-//    'Oficinas' => array('index'),
-//    'Manage',
-//);
-//
-//$this->menu = array(
-//    array('label' => 'List Oficina', 'url' => array('index')),
-//    array('label' => 'Create Oficina', 'url' => array('create')),
-//);
-//Yii::app()->clientScript->registerScript('search', "
-//$('.search-button').click(function(){
-//$('.search-form').toggle();
-//return false;
-//});
-//$('.search-form form').submit(function(){
-//$.fn.yiiGridView.update('oficina-grid', {
-//data: $(this).serialize()
-//});
-//return false;
-//});
-//");
+$this->breadcrumbs = array(
+    'Oficinas' => array('index'),
+    'Manage',
+);
+
+$this->menu = array(
+    array('label' => 'List Oficina', 'url' => array('index')),
+    array('label' => 'Create Oficina', 'url' => array('create')),
+);
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+$('.search-form').toggle();
+return false;
+});
+$('.search-form form').submit(function(){
+$.fn.yiiGridView.update('oficina-grid', {
+data: $(this).serialize()
+});
+return false;
+});
+");
 ?>
 
 <h1>Listado Oficina</h1>
@@ -35,10 +35,20 @@
 <!--</div> search-form -->
 
 <?php
-//$this->widget('booster.widgets.TbGridView', array(
-//    'id' => 'oficina-grid',
-//    'dataProvider' => $model->search(),
-//    'filter' => $model,
+function nombre($selec,$iD){
+    $saime = ConsultaOracle::getPersonaByPk($selec,(int)$iD);
+    return $saime['PRIMER_NOMBRE'];
+}
+function apellido($selec,$iD){
+    $saime = ConsultaOracle::getPersonaByPk($selec,(int)$iD);
+    return $saime['PRIMER_APELLIDO'];
+}
+
+$this->widget('booster.widgets.TbGridView', array(
+    'id' => 'oficina-grid',
+    'type' => 'striped bordered condensed',
+    'dataProvider' => $model->search(),
+    'filter' => $model,
 //    'columns' => array(
 //        'id_oficina',
 //        'nombre',
@@ -56,20 +66,72 @@
 //            'class' => 'booster.widgets.TbButtonColumn',
 //        ),
 //    ),
-//));
-?>
-<?php
-function nombre($selec,$iD){
-    $saime = ConsultaOracle::getPersonaByPk($selec,(int)$iD);
-    return $saime['PRIMER_NOMBRE'];
-}
-function apellido($selec,$iD){
-    $saime = ConsultaOracle::getPersonaByPk($selec,(int)$iD);
-    return $saime['PRIMER_APELLIDO'];
-}
+    
+'columns' => array(
+    'id_oficina' => array(
+    'header' => 'N°',
+    'name' => 'id_oficina',
+    'value' => '$data->id_oficina',
+    //'htmlOptions' => array('width' => '80', 'style' => 'text-align: center;'),
+
+    ),
+    'nombre' => array(
+    'header' => 'Nombre de Oficina',
+    'name' => 'nombre',
+    'value' => '$data->nombre',
+    //'htmlOptions' => array('width' => '80', 'style' => 'text-align: center;'),
+
+    ),
+    'primer_nombre' => array(
+    'header' => 'Nombre',
+    'name' => 'primer_nombre',
+    'value' => 'nombre("PRIMER_NOMBRE",$data->persona_id_jefe)',
+    
+    //'htmlOptions' => array('width' => '80', 'style' => 'text-align: center;'),
+
+    ),
+    
+    'primer_apellido' => array(
+    'header' => 'Apellido',
+    'name' => 'primer_apellido',
+    'value' => 'apellido("PRIMER_APELLIDO",$data->persona_id_jefe)',
+    // 'value' => '$data->persona_id_jefe',
+      ),
+    array(
+            'class' => 'booster.widgets.TbButtonColumn',
+            'header' => 'Acciones',
+            'htmlOptions' => array('width' => '85', 'style' => 'text-align: center;'),
+            'template' => '{ver} {modificar} {pdf}',
+            'buttons' => array(
+                'ver' => array(
+                    'label' => 'Ver',
+                    'icon' => 'eye-open',
+                    'size' => 'medium',
+                    'url' => 'Yii::app()->createUrl("oficina/view/", array("id"=>$data->id_oficina))',
+                ),
+                'modificar' => array(
+                    'label' => 'Modificar',
+                    'icon' => 'glyphicon glyphicon-pencil',
+                    'size' => 'medium',
+                    'url' => 'Yii::app()->createUrl("oficina/update/", array("id"=>$data->id_oficina))',
+//                    'visible' => 'Asignar($data->username);'
+                ),
+                'pdf' => array(
+                    'label' => 'Generar PDF',
+                    'icon'  => 'glyphicon glyphicon-file',
+                    'size'  => 'medium',
+                    'url'   => 'Yii::app()->createUrl("oficina/pdf/", array("id"=>$data->id_oficina))',
+//                    'visible' => 'Asignar($data->username);'
+                ),
+                
+            ),
+        ),
+    
+)));
 ?>
 
-<?php
+
+<?php /*
 $this->widget('booster.widgets.TbGridView', array(
     'id' => 'oficina-grid',
     'dataProvider' => $model->search(),
@@ -127,4 +189,4 @@ $this->widget('booster.widgets.TbGridView', array(
         ),
     ),
 ));
-?>
+*/?>
--- protocolizacion/protected/views/oficina/update.php
+++ protocolizacion/protected/views/oficina/update.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
 $this->breadcrumbs=array(
 	'Oficinas'=>array('index'),
 	$model->id_oficina=>array('view','id'=>$model->id_oficina),
@@ -13,6 +13,140 @@ $this->breadcrumbs=array(
 	);
 	?>
 
-	<h1>Update Oficina <?php echo $model->id_oficina; ?></h1>
+	<h1>Editar Oficina <?php echo $model->id_oficina; ?></h1>
 
-<?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
+<?php echo $this->renderPartial('_form',array('model'=>$model)); 
+ */ ?>
+
+<?php
+$form = $this->beginWidget('booster.widgets.TbActiveForm', array(
+    'id' => 'oficina-form',
+    'enableAjaxValidation' => false,
+    'enableClientValidation' => true,
+    'clientOptions' => array(
+        'validateOnSubmit' => true,
+        'validateOnChange' => true,
+        'validateOnType' => true,
+    ),
+        ));
+?>
+<?php
+    
+if (!empty($model->parroquia_id)) {
+    $id_parroquia = Tblparroquia::model()->findByPk($model->parroquia_id); // consulta en la tabla ciudad el id_ciudad y id_estado 
+    $id_municipio = $id_parroquia->clvmunicipio0->clvcodigo;
+    $id_estado = $id_parroquia->clvmunicipio0->clvestado0->clvcodigo;
+
+}
+
+?>
+<?php Yii::app()->clientScript->registerScript('oficina', "
+         $('#guardar').click(function(){
+                if($('#Oficina_nombre').val()==''){
+                   bootbox.alert('Por favor indique el nombre de la Oficina');
+                    return false;
+                }
+
+                if($('#Tblestado_clvcodigo').val()==''){
+                 alert('Por favor seleccione Estado');
+                    return false;
+                }
+                if($('#Tblmunicipio_clvcodigo').val()==''){
+                   alert('Por favor seleccione Municipio');
+                    return false;
+                }
+                if($('#Oficina_parroquia_id').val()==''){
+                   alert('Por favor seleccione Parroquia');
+                    return false;
+                }
+
+                });
+
+        $(document).ready(function(){
+            $('#Tblestado_clvcodigo').val(" . $id_estado . ");
+                    
+            $.get('" . CController::createUrl('ValidacionJs/BuscarMunicipios') . "', {clvcodigo: " . $id_estado . " }, function(data){
+                $('#Tblmunicipio_clvcodigo').html(data);
+                $('#Tblmunicipio_clvcodigo').val(" . $id_municipio . ");
+                
+            });
+            $.get('" . CController::createUrl('ValidacionJs/BuscarParroquias') . "', {municipio: " . $id_municipio . "}, function(data){
+                $('#Oficina_parroquia_id').html(data);
+                $('#Oficina_parroquia_id').val(" . $model->parroquia_id. ");
+            });
+        });
+
+
+        ") ?>
+
+<?php
+if (isset($sms) && !empty($sms)) {
+    $user = Yii::app()->getComponent('user');
+    $user->setFlash(
+            'warning', "<strong>Ya existe una Oficina con este nombre.</strong>"
+    );
+    $this->widget('booster.widgets.TbAlert', array(
+        'fade' => true,
+        'closeText' => '&times;', // false equals no close link
+        'events' => array(),
+        'htmlOptions' => array(),
+        'userComponentId' => 'user',
+        'alerts' => array(// configurations per alert type
+            'warning' => array('closeText' => false),
+        ),
+    ));
+}
+?>
+
+<h1>Oficina</h1>
+
+<div class="row">
+    <div class="col-md-12">
+        <?php
+        $this->widget(
+                'booster.widgets.TbPanel', array(
+            'title' => 'Caracteristica de la Oficina',
+            'context' => 'danger',
+            // 'headerHtmlOptions' => array('style' => 'background:url(' . Yii::app()->request->baseUrl . '/img/fondo_barra.jpg);color:white;'),
+            'headerIcon' => 'globe',
+            'content' => $this->renderPartial('_form', array('form' => $form, 'model' => $model, 'estado' => $estado, 'municipio' => $municipio, 'parroquia' => $parroquia), TRUE),
+                )
+        );
+        ?>
+    </div>
+</div>
+
+<div class="well">
+    <div class="pull-center" style="text-align: right;">
+			<?php
+			$this->widget('booster.widgets.TbButton', array(
+					'buttonType' => 'submit',
+					'icon' => 'glyphicon glyphicon glyphicon-step-backward',
+					'size' => 'large',
+					'id' => 'cancelar',
+					'context' => 'danger',
+					'label' => 'Cancelar',
+					//'url' => $this->createURL('/oficina/admin'),
+					'htmlOptions' => array(
+		'onclick' => 'document.location.href ="' . $this->createUrl('oficina/admin') . '"'),
+
+			));
+			?>
+			<?php
+        $this->widget('booster.widgets.TbButton', array(
+            'buttonType' => 'submit',
+            'icon' => 'glyphicon glyphicon-floppy-saved',
+            'size' => 'large',
+            'id' => 'guardar',
+            'context' => 'primary',
+            'label' => $model->isNewRecord ? 'Guardar' : 'Actualizar',
+        ));
+        ?>
+    </div>
+</div>
+
+<?php //echo $this->renderPartial('_form', array('model'=>$model));  ?>
+
+<?php $this->endWidget(); ?>
+
+ 
