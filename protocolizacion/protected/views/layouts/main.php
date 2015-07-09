eec5b829157a6c8b08efdbb7488b5438b7a3753f
desarrollo <desarrollo@desarrollo>
2015-07-08 21:51:17 -0430

    sin merge

--- protocolizacion/protected/views/layouts/main.php
+++ protocolizacion/protected/views/layouts/main.php
@@ -19,7 +19,6 @@
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/menu.css" />
 
-
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
     </head>
     <body>
