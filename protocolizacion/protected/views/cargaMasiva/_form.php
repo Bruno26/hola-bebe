4f3412e5a113b1c59c11ba944643f7b31c858535
esmir <esmir@esmir>
2015-07-28 15:44:08 -0430

    update carga masiva

--- protocolizacion/protected/views/cargaMasiva/_form.php
+++ protocolizacion/protected/views/cargaMasiva/_form.php
@@ -1,72 +1,125 @@
- <?php
- $form=$this->beginWidget('booster.widgets.TbActiveForm',array(
-	'id'=>'carga-masiva-form',
-	'enableAjaxValidation'=>false,
-    //'enableAjaxValidation' =>false,
-       'enableClientValidation'=>true,
-        'clientOptions' => array(
-        'validateOnSubmit' => true,
-        ),
-     'type'=>'horizontal',
-     'htmlOptions' => array('enctype' => 'multipart/form-data'),
-));
 
- ?>
-	 <div class="alert alert-info"><i class="icon-info-sign"></i> Para realizar la carga masiva de adjudicados adjunte el archivo .csv que desea importar dando clic en el boton "Examinar". Una vez seleccionado el archivo de clic en el boton "Subir archivo"; el sistema evaluará que el archivo cumpla con todos los parametros requeridos y procedera a guardar en el sistema.</div>
+<div class="alert alert-info"><i class="icon-info-sign"></i><h4><center> Para realizar la carga masiva de adjudicados adjunte el <b>archivo .csv</b> que desea importar dando clic en el boton <b>"Examinar"</b>. Una vez seleccionado el archivo de clic en el boton <b>"Subir archivo"</b>; el sistema evaluará que el archivo cumpla con todos los parametros requeridos y procedera a guardar en el sistema.</center></h4></div>
 
-	 <p class="help-block"><span class="required">*</span>Campos de llenado obligatorio.</p></br>
-	 <?php
-	 $this->widget('booster.widgets.TbAlert', array(
-  //  'block' => true, // display a larger alert block?
+<p class="help-block">Los campos marcados con <span class="required">*</span> son requeridos.</p><br>
+
+<div class="row">
+    <div class="col-md-4">
+
+        <?php
+        $criteria = new CDbCriteria;
+        $criteria->order = 'strdescripcion ASC';
+        echo $form->dropDownListGroup($estado, 'clvcodigo', array('wrapperHtmlOptions' => array('class' => 'col-sm-4',),
+            'widgetOptions' => array('data' => CHtml::listData(Tblestado::model()->findAll($criteria), 'clvcodigo', 'strdescripcion'),
+                'htmlOptions' => array(
+                    'empty' => 'SELECCIONE',
+                    'ajax' => array(
+                        'type' => 'POST', 'url' => CController::createUrl('ValidacionJs/BuscarMunicipios'),
+                        'update' => '#' . CHtml::activeId($municipio, 'clvcodigo'),
+                    ),
+                ),
+            )
+                )
+        );
+        ?>
+    </div>
+    <div class="col-md-4">
+        <?php
+        echo $form->dropDownListGroup($municipio, 'clvcodigo', array('wrapperHtmlOptions' => array('class' => 'col-sm-12',),
+            'widgetOptions' => array(
+                'htmlOptions' => array(
+                    'ajax' => array(
+                        'type' => 'POST',
+                        'url' => CController::createUrl('ValidacionJs/BuscarParroquias'),
+                        'update' => '#' . CHtml::activeId($parroquia, 'clvcodigo'),
+                    ),
+                    'empty' => 'SELECCIONE',
+                ),
+            )
+                )
+        );
+        ?>
+    </div>
+    <div class="col-md-4">
+
+        <?php
+        echo $form->dropDownListGroup($parroquia, 'clvcodigo', array('wrapperHtmlOptions' => array('class' => 'col-sm-12 limpiar',),
+            'widgetOptions' => array('htmlOptions' => array(
+                    'ajax' => array(
+                        'type' => 'POST',
+                        'url' => CController::createUrl('ValidacionJs/BuscarDesarrollo'),
+                        'update' => '#' . CHtml::activeId($model, 'desarrollo_id'),
+                    ),
+                    'empty' => 'SELECCIONE',
+                ),
+            )
+                )
+        );
+        ?>
+    </div>
+
+</div>
+<div class="row">
+    <div class="col-md-4">
+
+        <?php echo $form->textFieldGroup($desarrollo, 'nombre', array('widgetOptions' => array('htmlOptions' => array('class' => 'span5')))); ?>
+    </div>
+   
+
+</div>
+
+<?php
+$this->widget('booster.widgets.TbAlert', array(
+    //  'block' => true, // display a larger alert block?
     'fade' => true, // use transitions?
     'closeText' => '×', // close link text - if set to false, no close link is displayed
     'alerts' => array(// configurations per alert type
-    'error' => array('block' => true, 'fade' => true, 'closeText' => '×'), // success, info, warning, error or danger
+        'error' => array('block' => true, 'fade' => true, 'closeText' => '×'), // success, info, warning, error or danger
     ),
 ));
+?>
 
-	 ?>
-         <?php echo $form->errorSummary($model); ?>
-         <fieldset>
-         <legend>Buscar archivo</legend>
-         <div class='row'>
-            <div class='span4'>
-               <?php echo CHtml::label('<b>Seleccione archivo csv</b> <span class="required"> *</span>','');?>
-               <?php
-                echo $form->fileField($model,'nombre_archivo');
-		echo $form->error($model, 'nombre_archivo');
+<?php echo $form->errorSummary($model); ?>
+<fieldset>
+    <legend>Buscar archivo</legend>
+    <div class='well'>
+        <div class='span4'>
+            <?php echo CHtml::label('<b>Seleccione archivo csv</b> <span class="required"> *</span>', ''); ?>
+            <?php
+            echo $form->fileField($model, 'nombre_archivo');
+            echo $form->error($model, 'nombre_archivo');
+            ?>
+        </div>
+    </div>
+</fieldset>
+<br><br>
 
-               ?>
-            </div>
-         </div>
-         </fieldset>
-         <br /><br />
+<p align='center'> <a title="Descargar" href='doc/carga_masiva.csv'>
+        <b>El archivo a subir debe de ajustarse al siguiente formato .csv </b>
+    </a> </p>
 
-				<p align='center'> <a title="Descargar" href='doc/carga_masiva.csv'>
-						<b>El archivo a subir debe de ajustarse al siguiente formato .csv </b>
-          </a> </p>
-
-         <div class="form-actions">
-		<?php
-		$this->widget('booster.widgets.TbButton', array(
-			'buttonType'=>'submit',
-		//	'type'=>'success',
-			'label'=>'Subir archivo',
-		)); ?>
-
-
-		<?php
-		    $this->widget('booster.widgets.TbButton',array(
-		   //'type'=>'primary',
-		   'label'=>'Regresar',
-		   'buttonType' =>'link',
-		   'url'=> CHtml::normalizeUrl(array('admin')),
-		));
-		?>
-
-	</div>
+<div class="form-actions">
+    <?php
+    $this->widget('booster.widgets.TbButton', array(
+        'buttonType' => 'submit',
+        //	'type'=>'success',
+        'id' => 'subirArchivo',
+        'label' => 'Subir archivo',
+    ));
+    ?>
 
 
+    <?php
+    $this->widget('booster.widgets.TbButton', array(
+        //'type'=>'primary',
+        'label' => 'Regresar',
+        'buttonType' => 'link',
+        'url' => CHtml::normalizeUrl(array('admin')),
+    ));
+    ?>
+
+</div>
 
 
-<?php $this->endWidget(); ?>
+
+
--- protocolizacion/protected/views/cargaMasiva/create.php
+++ protocolizacion/protected/views/cargaMasiva/create.php
@@ -1,25 +1,61 @@
 <?php
-$this->breadcrumbs=array(
-	'Carga Masivas'=>array('index'),
-	'Create',
-);
+$form = $this->beginWidget('booster.widgets.TbActiveForm', array(
+    'id' => 'carga-masiva-form',
+    'enableAjaxValidation' => false,
+    //'enableAjaxValidation' =>false,
+    'enableClientValidation' => true,
+    'clientOptions' => array(
+        'validateOnSubmit' => true,
+    ),
+//    'type' => 'horizontal',
+    'htmlOptions' => array('enctype' => 'multipart/form-data'),
+        ));
 ?>
+<?php Yii::app()->clientScript->registerScript('desarrollo', "
+         $('#subirArchivo').click(function(){
+         
+                if ($('#Tblestado_clvcodigo').val()==''){
+                    bootbox.alert('Por favor seleccione estado');
+                    return false;
+                }
+                if ($('#Tblmunicipio_clvcodigo').val()==''){
+                    bootbox.alert('Por favor seleccione municipio');
+                    return false;
+                }
+                if ($('#Tblparroquia_clvcodigo').val()==''){
+                    bootbox.alert('Por favor seleccione parroquia');
+                    return false;
+                }
+                if ($('#Desarrollo_nombre').val()==''){
+                    bootbox.alert('Por favor seleccione el nombre del desarrollo');
+                    return false;
+                }
+                if ($('#CargaMasiva_nombre_archivo').val()==''){
+                    bootbox.alert('Por favor seleccione un archivo.csv');
+                    return false;
+                }
+                
+            });
+
+") ?>
 
 <h1>Realizar Carga Masiva</h1>
 
-<?php //echo $this->renderPartial('_form', array('model'=>$model)); ?>
 
 <div class="row">
     <div class="col-md-12">
         <?php
         $this->widget(
-            'booster.widgets.TbPanel', array(
-            'title' => 'Carga Masiva - ',
+                'booster.widgets.TbPanel', array(
+            'title' => 'Carga Masiva',
             'context' => 'info',
             'headerHtmlOptions' => array('style' => 'background: linear-gradient(#1fb5ad, #1fd0ff) !important;color: #FFFFFF !important;'),
             'headerIcon' => 'glyphicon glyphicon-list-alt',
-            'content' => $this->renderPartial('_form', array( 'model' => $model, 'estado' => $estado, 'municipio' => $municipio, 'parroquia' => $parroquia, 'desarrollo' => $desarrollo ), TRUE),)
+            'content' => $this->renderPartial('_form', array('model' => $model, 'estado' => $estado, 'municipio' => $municipio, 'parroquia' => $parroquia, 'desarrollo' => $desarrollo, 'form'=>$form,), TRUE),)
         );
         ?>
     </div>
 </div>
+
+
+<?php $this->endWidget(); ?>
