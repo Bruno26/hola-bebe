fdc87ad8b04b8e1c61d07ec229d6f96df8bc4736
Bruno Palacios <desarrollo@desarrollo>
2015-08-01 20:21:45 -0430

    agregando calculos de analisis de credito

--- protocolizacion/protected/controllers/CalculoAnalisisCredito/CalculosController.php
+++ protocolizacion/protected/controllers/CalculoAnalisisCredito/CalculosController.php
@@ -16,7 +16,7 @@ class CalculosController extends Controller {
     public function accessRules() {
         return array(
             array('allow', // allow all users to perform 'index' and 'view' actions
-                'actions' => array('CalculoTasaAmortizacion'),
+                'actions' => array('CuotaFinancieraMaxima','MaximaCapacidadPago', 'CreditoSolicitado'),
                 'users' => array('*'),
             ),
             array('deny', // deny all users
@@ -26,14 +26,39 @@ class CalculosController extends Controller {
     }
 
     /*
-     * 
+     * Cuota Finaciera Máxima (35% IFM)
      */
 
-    public function actionCalculoTasaAmortizacion() {
-        //echo CJSON::encode(1);
-        echo '<pre>';
-        var_dump($_POST);
-        die;
+    public function actionCuotaFinancieraMaxima($ingresoFamiliar) {
+        return $ingresoFamiliar * 0.35;
+    }
+
+    /*
+     * Maxima Capacidad Pago
+     */
+
+    public function actionMaximaCapacidadPago($interes, $anhios, $CuotaFinanaciadaMaxima) {
+	$I = $interes / 12 /100;
+	$PREEXP = 1 + $I;
+	$EXP = pow($PREEXP, -($anhios * 12));
+	
+	$MCP = $CuotaFinanaciadaMaxima * (1 - $EXP ) / $I;
+	return number_format($MCP, 2, ',', '');
+        
+    }
+    /*
+     * Monto Credito Solicitado
+     */
+
+    public function actionCreditoSolicitadp($inicial, $costoViviendo) {
+	return $costoViviendo - $inicial;
+    }
+    /*
+     * Cuota Inicial Fondo de Garantía (1,43%)
+     */
+
+    public function actionCreditoSolicitado($CreditoSolicitado) {
+	return $CreditoSolicitado * 0.0143 ;
     }
 
 }
